c8e1dd599ea041e74ed5af2dd724d81d6a7fee2d
김민우 <kmw6624@naver.com>
2024-05-17 11:50:44 +0900

    Update_Input System
    Behavior Controller, Input Controller  스크립트 수정

--- Assets/Scripts/Unit/BehaviorController.cs
+++ Assets/Scripts/Unit/BehaviorController.cs
@@ -6,9 +6,15 @@ using UnityEngine;
 public class BehaviorController : MonoBehaviour
 {
     public event Action<Vector2> OnMoveEvent;
+    public event Action<Vector2> OnLookEvent;
 
     public void CallMoveEvent(Vector2 input)
     {
         OnMoveEvent?.Invoke(input);
+    }
+
+    public void CallLookEvent(Vector2 input)
+    {
+        OnLookEvent?.Invoke(input);
     }
 }
--- Assets/Scripts/Unit/Player/InputController.cs
+++ Assets/Scripts/Unit/Player/InputController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.U2D.Animation;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -9,5 +10,14 @@ public class InputController : BehaviorController
     {
         Vector2 direction = input.ReadValue<Vector2>().normalized;
         CallMoveEvent(direction);
+    }
+
+    public void OnLook(InputAction.CallbackContext input)
+    {
+        Vector2 direction = input.ReadValue<Vector2>().normalized;
+        Vector2 worldPos = Camera.main.ScreenToWorldPoint(direction);
+        direction = (worldPos - (Vector2)transform.position).normalized;
+
+        CallLookEvent(direction);
     }
 }
