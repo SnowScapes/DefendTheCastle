444ff355090f44a0d6e2ec269a9136cafb5d694f
김민우 <kmw6624@naver.com>
2024-05-20 00:23:54 +0900

    Fix_UnitStat.cs
    1) field:SerializeField 추가해서 Inspector 창에 뜨게 수정
    2) Player 프리팹에 attackSO 추가

--- Assets/Scripts/Unit/UnitStat.cs
+++ Assets/Scripts/Unit/UnitStat.cs
@@ -3,17 +3,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[Serializable]
-public class UnitStat : MonoBehaviour
+public class UnitStat
 {
-    public int id { get; set; }
-    public string unitName { get; set; }
-    public int maxHp { get; set; }
-    public int hp { get; set; }
-    public float moveSpeed { get; set; }
-    public float attackSpeed { get; set; }
-    public int atk { get; set; }
-    public float range { get; set; }
-    public int level { get; set; }
-    public int gold { get; set; }
+    [field: SerializeField] public int id { get; set; }
+    [field: SerializeField] public string unitName { get; set; }
+    [field: SerializeField] public int maxHp { get; set; }
+    [field: SerializeField] public int hp { get; set; }
+    [field: SerializeField] public float moveSpeed { get; set; }
+    [field: SerializeField] public float attackSpeed { get; set; }
+    [field: SerializeField] public int atk { get; set; }
+    [field: SerializeField] public float range { get; set; }
+    [field: SerializeField] public int level { get; set; }
+    [field: SerializeField] public int gold { get; set; }
 }
