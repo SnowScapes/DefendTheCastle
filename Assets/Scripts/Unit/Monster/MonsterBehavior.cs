a037694c1987576d887d028f496161927b67bd92
SnowScapes <siwon2113@gmail.com>
2024-05-20 17:27:13 +0900

    Move SetRallyPos Method to MonsterSpawner

--- Assets/Scripts/Unit/Monster/MonsterBehavior.cs
+++ Assets/Scripts/Unit/Monster/MonsterBehavior.cs
@@ -11,6 +11,7 @@ public class MonsterBehavior : BehaviorController
     [SerializeField] private Rigidbody2D _rigidbody;
     [SerializeField] private MonsterSpawner spawnmanager;
     [SerializeField] internal Spawn spawnPoint;
+    [SerializeField] private MonsterStat stat;
 
     private IEnumerator move;
     private Queue<Vector2> rallyPoint = new Queue<Vector2>();
@@ -18,36 +19,18 @@ public class MonsterBehavior : BehaviorController
 
     protected virtual void Awake()
     {
-        spawnmanager = GameObject.Find("SpawnManager").GetComponent<MonsterSpawner>();
         _rigidbody = this.GetComponent<Rigidbody2D>();
         _transform = this.GetComponent<Transform>();
     }
 
     protected virtual void Start()
     {
-        SetRallyPos();
         OnMoveEvent += WalkTo;
         move = moveCoroutine();
         StartCoroutine(move);
     }
 
-    private void SetRallyPos()
-    {
-        switch (spawnPoint)
-        {
-            case Spawn.Left:
-                queueRally(spawnmanager.Rally_Left);
-                break;
-            case Spawn.Center: 
-                queueRally(spawnmanager.Rally_Center);
-                break;
-            case Spawn.Right: 
-                queueRally(spawnmanager.Rally_Right);
-                break;
-        }
-    }
-
-    private void queueRally(List<Transform> rallyPos)
+    public void queueRally(List<Transform> rallyPos)
     {
         for (int i = 0; i < rallyPos.Count; i++)
         {
@@ -59,23 +42,35 @@ public class MonsterBehavior : BehaviorController
     
     private void WalkTo(Vector2 destination)
     {
-        // 추후에 정보에서 속도 불러오기
-        _rigidbody.position = Vector2.MoveTowards(_rigidbody.position, destination, 0.01f);
+        _rigidbody.position = Vector2.MoveTowards(_rigidbody.position, destination, Time.deltaTime);
         if ((Vector2)_rigidbody.position == destination)
-            arrived = true;
-    }
-
-    protected void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.gameObject.CompareTag("Player"))
         {
-            StopCoroutine(move);
+            arrived = true;
         }
     }
 
-    protected void OnTriggerExit2D(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        StartCoroutine(move);
+        switch (other.gameObject.layer)
+        {
+            case 11: Debug.Log("Attack Player");
+                StopCoroutine(move);
+                CallAttackEvent(stat.attackSO);
+                break;
+            case 12: Debug.Log("Attack Castle");
+                StopCoroutine(move);
+                CallAttackEvent(stat.attackSO);
+                break;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            StartCoroutine(move);
+            Debug.Log("Goto Castle Again");
+        }
     }
 
     IEnumerator moveCoroutine()
