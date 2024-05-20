9670d5da59dbab7af682bbafb5a6430bd1b6cee8
SnowScapes <siwon2113@gmail.com>
2024-05-20 17:27:55 +0900

    Update Features in MonsterAnimationController

--- Assets/Scripts/Unit/Monster/MonsterAnimationController.cs
+++ Assets/Scripts/Unit/Monster/MonsterAnimationController.cs
@@ -1,18 +1,43 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class MonsterAnimationController : PlayerAnimationController
+public class MonsterAnimationController : AnimationController
 {
     private static readonly int attack = Animator.StringToHash("attack");
+    private Transform _transform;
+    private MonsterBehavior _monsterBehavior;
 
-    private void Awake()
+    protected override void Awake()
     {
         base.Awake();
+        _transform = GetComponent<Transform>();
+        _monsterBehavior = GetComponent<MonsterBehavior>();
     }
 
-    protected override void Attacking()
+    private void Start()
+    {
+        controller.OnAttackEvent += Attacking;
+        controller.OnMoveEvent += MoveAnimation;
+    }
+
+    private void Attacking(AttackSO attackSo)
     {
         animator.SetTrigger(attack);
+    }
+
+    private void MoveAnimation(Vector2 direction)
+    {
+        if ((Vector2)transform.position != direction)
+            animator.SetBool("isRun", true);
+        else
+            animator.SetBool("isRun", false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        animator.SetBool("isRun", false);
     }
 }
